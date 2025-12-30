1a6ffc5ee8cf8b296896c732eb7affc215fc1f6f
Robert Ball <jhrobert@pm.me>
2025-12-30 13:06:52 -0800

    update snacks: add lua formatting

--- config/utilities/snacks/keymaps.nix
+++ config/utilities/snacks/keymaps.nix
@@ -4,49 +4,65 @@
     {
       mode = "n";
       key = "<leader><space>";
-      action.__raw = "function() Snacks.picker.smart() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.smart() end";
       options.desc = "Find Files";
     }
     {
       mode = "n";
       key = "<leader>,";
-      action.__raw = "function() Snacks.picker.buffers() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.buffers() end";
       options.desc = "Buffers";
     }
     {
       mode = "n";
       key = "<leader>/";
-      action.__raw = "function() Snacks.picker.grep() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.grep() end";
       options.desc = "Search Files";
     }
     {
       mode = "n";
       key = "<leader>n";
-      action.__raw = "function() Snacks.picker.notifications() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.notifications() end";
       options.desc = "Notifications";
     }
     {
       mode = "n";
       key = "<leader>e";
-      action.__raw = "function() Snacks.picker.explorer() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.explorer() end";
       options.desc = "File Explorer";
     }
     {
       mode = "n";
       key = "<leader>ff";
-      action.__raw = "function() Snacks.picker.files() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.files() end";
       options.desc = "Find Files";
     }
     {
       mode = "n";
       key = "<leader>cl";
-      action.__raw = "function() Snacks.picker.lsp_config() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.lsp_config() end";
       options.desc = "Lsp Info";
     }
     {
       mode = ["n" "x"];
       key = "gd";
-      action.__raw = "function() Snacks.picker.lsp_definitions() end";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.lsp_definitions() end";
       options.desc = "Goto Definition";
     }
 
@@ -54,19 +70,25 @@
     {
       mode = ["n" "t"];
       key = "<C-/>";
-      action.__raw = "function() Snacks.terminal() end";
+      action.__raw =
+        #lua
+        "function() Snacks.terminal() end";
       options.desc = "Terminal";
     }
     {
       mode = "n";
       key = "<leader>ft";
-      action.__raw = "function() Snacks.terminal() end";
+      action.__raw =
+        #lua
+        "function() Snacks.terminal() end";
       options.desc = "Terminal";
     }
     {
       mode = "n";
       key = "<leader>gg";
-      action.__raw = "function() Snacks.terminal('lazygit') end";
+      action.__raw =
+        #lua
+        "function() Snacks.terminal('lazygit') end";
       options.desc = "LazyGit";
     }
 
@@ -74,13 +96,17 @@
     {
       mode = "n";
       key = "<leader>bd";
-      action.__raw = "function() Snacks.bufdelete() end";
+      action.__raw =
+        #lua
+        "function() Snacks.bufdelete() end";
       options.desc = "Delete Buffer";
     }
     {
       mode = "n";
       key = "<leader>bo";
-      action.__raw = "function() Snacks.bufdelete.other() end";
+      action.__raw =
+        #lua
+        "function() Snacks.bufdelete.other() end";
       options.desc = "Delete Other Buffers";
     }
     {
@@ -88,6 +114,52 @@
       key = "<leader>bD";
       action = "<cmd>:bd<cr>";
       options.desc = "Delete Buffer and Window";
+    }
+
+    # Git
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action.__raw =
+        #lua
+        "function() Snacks.git.blame_line() end";
+      options.desc = "Git Blame Line";
+    }
+    {
+      mode = "n";
+      key = "<leader>gd";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.git_diff() end";
+      options.desc = "Git Diff (hunks)";
+    }
+    {
+      mode = "n";
+      key = "<leader>gD";
+      action.__raw =
+        #lua
+        ''
+          function()
+            Snacks.picker.git_diff({base = "origin", group = true})
+          end
+        '';
+      options.desc = "Git Diff (origin)";
+    }
+    {
+      mode = "n";
+      key = "<leader>gs";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.git_status() end";
+      options.desc = "Git Status";
+    }
+    {
+      mode = "n";
+      key = "<leader>gS";
+      action.__raw =
+        #lua
+        "function() Snacks.picker.git_stash() end";
+      options.desc = "Git Stash";
     }
   ];
 }
