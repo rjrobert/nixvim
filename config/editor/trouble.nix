0438f27147d4386b7acba4f08ebb3f374c37d6e7
Robert Ball <jhrobert@pm.me>
2025-12-31 14:58:02 -0800

    trouble keymaps

--- config/editor/trouble.nix
+++ config/editor/trouble.nix
@@ -43,5 +43,53 @@
       action = "<cmd>Trouble symbols toggle focus=false<cr>";
       options.desc = "Symbols (Trouble)";
     }
+    {
+      mode = "n";
+      key = "<leader>xL";
+      action = "<cmd>Trouble loclist toggle<cr>";
+      options.desc = "Location List (Trouble)";
+    }
+    {
+      mode = "n";
+      key = "<leader>xQ";
+      action = "<cmd>Trouble qflist toggle<cr>";
+      options.desc = "Quickfix List (Trouble)";
+    }
+    {
+      mode = "n";
+      key = "[q";
+      action.__raw =
+        #lua
+        ''
+          function()
+            if require("trouble").is_open() then
+              require("trouble").prev({skip_groups = true, jump = true})
+            else
+              local ok, err = pcall(vim.cmd.cprev)
+              if not ok then
+                vim.notify(err, vim.log.levels.ERROR)
+              end
+            end
+        '';
+      options.desc = "Previous Trouble/Quickfix Item";
+    }
+    {
+      mode = "n";
+      key = "]q";
+      action.__raw =
+        #lua
+        ''
+          function()
+            if require("trouble").is_open() then
+              require("trouble").next({skip_groups = true, jump = true})
+            else
+              local ok, err = pcall(vim.cmd.cnext)
+              if not ok then
+                vim.notify(err, vim.log.levels.ERROR)
+              end
+            end
+        '';
+      options.desc = "Next Trouble/Quickfix Item";
+    }
   ];
 }
