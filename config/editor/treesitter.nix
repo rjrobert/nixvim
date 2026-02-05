59074caa48e202e59c58f08ce66a7fe996567338
Robert Ball <jhrobert@pm.me>
2026-02-05 11:50:43 -0800

    update treesitter

--- config/editor/treesitter.nix
+++ config/editor/treesitter.nix
@@ -1,44 +1,10 @@
-{
+{...}: {
   plugins = {
     treesitter = {
       enable = true;
       highlight.enable = true;
       indent.enable = true;
       folding.enable = true;
-      settings = {
-        highlight = {
-          enable = true;
-          additional_vim_regex_highlighting = false;
-        };
-      };
-      nixvimInjections = true;
-    };
-
-    treesitter-textobjects = {
-      enable = true;
-      settings = {
-        select = {
-          enable = true;
-          lookahead = true;
-          keymaps = {
-            "af" = "@function.outer";
-            "if" = "@function.inner";
-            "ac" = "@class.outer";
-            "ic" = "@class.inner";
-          };
-        };
-        move = {
-          enable = true;
-          gotoNextStart = {
-            "]f" = "@function.outer";
-            "]c" = "@class.outer";
-          };
-          gotoPreviousStart = {
-            "[f" = "@function.outer";
-            "[c" = "@class.outer";
-          };
-        };
-      };
     };
 
     ts-autotag.enable = true;
